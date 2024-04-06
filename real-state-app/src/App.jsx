f9d61dffa79b24b1e9248b3d2f6f2970fd0407a4
lawrarod <zayumlaura@gmail.com>
2024-04-06 16:45:36 +0200

    updates

--- real-state-app/src/App.jsx
+++ real-state-app/src/App.jsx
@@ -1,11 +1,18 @@
 import './layout.scss'
 import { Navbar } from './Components/Navbar/Navbar.jsx'
+import { HomePage } from './Routes/HomePage.jsx'
+
 
 function App() {
 
   return (
     <div className='layout'>
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
   )
 }
--- real-state-app/src/Components/Navbar/Navbar.jsx
+++ real-state-app/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,39 @@
+import { useState } from 'react'
 import './navbar.scss'
+// import geometrico from './geometrico.png'
 
 export const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+
     return (
         <nav>
-            <div className="left">Left</div>
-            <div className="right">Right</div>
+            <div className="left">
+                <a href="/" className='logo'>
+                    <img src='/assets/images/geometrico.png' />
+                    <span>FineEstate</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
+
+            <div className="right">
+                <a href="/">Sign In</a>
+                <a href="/" className='register'>Sign Up</a>
+                <div className="menuIcon">
+                    <img src="/assets/images/menu-icon.png" alt="hamburguer menu icon" onClick={() => setOpen(!open)} />
+                </div>
+                <div className={open ? 'menu active' : 'menu'}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sign In</a>
+                    <a href="/">Sign Up</a>
+                </div>
+            </div>
         </nav>
     )
 }
