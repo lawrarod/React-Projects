e41efed8a54b4b9e73efd928b73eee01ce88d513
lawrarod <zayumlaura@gmail.com>
2024-04-10 17:00:55 +0200

    styles added

--- prueba-react-gatitos/src/App.jsx
+++ prueba-react-gatitos/src/App.jsx
@@ -1,20 +1,24 @@
 import './App.css'
 import { useCatImage } from './hooks/useCatImage'
 import { useCatFact } from './hooks/useCatFact'
+import { useState } from 'react'
 
 function App() {
   const { fact, updateFact } = useCatFact()
   const { imageUrl } = useCatImage({ fact })
 
+  const [visible, setVisible] = useState(false)
+
   const handleClick = async () => {
     updateFact()
+    setVisible(visible)
   }
 
   return (
     <main>
       <h1>Kittens App</h1>
 
-      <section className="content">
+      <section className={'content' + visible && 'visible'}>
         {imageUrl && <img src={imageUrl} alt='random cat image' />}
         {fact && <p>{fact}</p>}
       </section>
